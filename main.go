b96be0ae6d3cdc26e6d22fc844885dee2818d899
rigon <2412789408@qq.com>
2025-01-29 18:25:29 +0800

    前缀路由树

--- main.go
+++ main.go
@@ -15,10 +15,13 @@ func main() {
 		c.String(http.StatusOK, "Hello %s, you're at %s\n", c.Query("name"), c.Path)
 	})
 
-	r.POST("/login", func(c *gee.Context) {
+	r.GET("/hello/:name", func(c *gee.Context) {
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+	})
+
+	r.GET("/assets/*filepath", func(c *gee.Context) {
 		c.JSON(http.StatusOK, gee.H{
-			"username": c.PostForm("username"),
-			"password": c.PostForm("password"),
+			"filepath": c.Param("filepath"),
 		})
 	})
 
